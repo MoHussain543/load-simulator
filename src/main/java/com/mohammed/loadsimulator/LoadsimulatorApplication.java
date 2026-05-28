94a19d63f03cee7ec4f028566bbe7de9256ec753
Mohammed Hussain <mohammadhussain9924@gmail.com>
2026-05-28 05:34:16 -0400

    Initial project setup

---
+++ src/main/java/com/mohammed/loadsimulator/LoadsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.mohammed.loadsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadsimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohammed/loadsimulator/LoadsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammed.loadsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
